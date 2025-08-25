729d186af63b31f32efb82c26e5c307697fa771e
bhard <bhard@LAPTOP-GUHAS95G>
2025-08-25 17:20:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DelegateExample
+{
+    
+    public delegate int MathOperation(int a, int b);
+
+    class Program
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+   
+            MathOperation operation = new MathOperation(Add);
+
+            int result = operation(10, 20);
+
+      
+            Console.WriteLine("The sum is: " + result);
+        }
+    }
+}
+
